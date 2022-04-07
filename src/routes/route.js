099464c126ec5cf46d682719f19be9391ecfe66a
SudhanshuSahu <sudhanshusahu1999@gmail.com>
2022-04-07 16:47:05 +0530

    Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const logger = require('../logger/logger')
 const router = express.Router();
 const formatter = require('../validator/formatter')
 const helper = require('../util/helper')
+const lodash = require('lodash');
 
 
 router.get('/test-me', function (req, res) {
@@ -17,27 +18,28 @@ router.get('/test-me', function (req, res) {
 });
 router.get('/hello', function (req, res) {
     res.send('My 2nd api');
-  let monthNames = ["January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"];
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    let subArrays = lodash.chunk(months, 3)
+    console.log('The result after splitting the months array is ', subArrays)
 
-  let _ = require("lodash");
-  console.log(_.chunk(monthNames, monthNames.length / 4));
+//   let _ = require("lodash");
+//   console.log(_.chunk(monthNames, monthNames.length / 4));
 
   let oddArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-  console.log(_.tail(oddArray))
+  console.log(lodash.tail(oddArray))
 
-  let unionFx = function (...a) {
-      return _.union(...a);
-  }
+//   let unionFx = function (...a) {
+//       return _.union(...a);
+//   }
   let a1 = [1, 2, 3, 4];
   let a2 = [3, 4, 5, 6];
   let a3 = [7, 8, 6, 7];
   let a4 = [9, 8, 5, 4];
   let a5 = [68, 7, 9, 68];
-  console.log(unionFx(a1, a2, a3, a4, a5));
+  console.log(lodash.union(a1, a2, a3, a4, a5));
 
   let moviesArr = [["horror", "The Shining"], ["drama", "Titanic"], ["thriller", "Shutter Island"], ["fantasy", "Pans Labyrinth"]]
-  console.log(_.fromPairs(moviesArr));
+  console.log(lodash.fromPairs(moviesArr));
   
 });
 
