1ee67c06bb64ccefac40e136e00e0078c0273cba
SudhanshuSahu <sudhanshusahu1999@gmail.com>
2022-04-07 18:58:58 +0530

    incomplete

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,20 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/all-candidates', function (req, res) {
+    // console.log('------------------')
+    // console.log(req)
+    // console.log('------------------')
+    // console.log('These are the request query parameters: ', req.query)
+    res.send('[Sudhanshu]  , [Aditya] , [Rupesh] , [prateek] , [vijay]')
+});
+router.get('/candidates', function (req, res) {
+const arr = ["a" , "b" , "c" , "d"]
+if(req.query.count !== 0 ||req.query.count <= 10)
+res.send()
+
+
+
 });
 
 
