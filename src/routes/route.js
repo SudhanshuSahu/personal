39888ef708540212fb6c5eccb8302cbf1f0f7cab
SudhanshuSahu <sudhanshusahu1999@gmail.com>
2022-04-08 17:19:42 +0530

    this is a session of post api and Json

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,34 +2,36 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+router.get('/test-api1', function (req, res) {
+    res.send('My first ever api! ')
 });
 
-router.get('/hi' , function (req , res) {
-    let prob1=function(){
-        let arr = [1, 2, 3, 4, 5, 7, 8, 9]
-        let sum = 0;
-        let missNum;
-        for (let i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
-        }
-        missNum = (((arr.length + 1) * (arr.length + 2)) / 2) - sum;
-        return 'missing number from the the array of natural numbers is: '+ missNum;
-    }
-    
-    // let prob2=function() {
-    //     let arr = [33, 34, 35, 37, 38, 39]
-    //     let sum = 0;
-    //     let missNum;
-    //     for (let i = 0; i < arr.length; i++) {
-    //         sum = sum + arr[i];
-    //     }
-    //     missNum = (((arr.length + 1) / 2 * (arr[0] + arr[arr.length - 1]))) - sum;
-    //     return 'missing number from the the array is: '+ missNum;
-    // }
-    res.send(prob1(missNum))
+router.get('/test-api2', function (req, res) {
+    res.send('My first ever api! ')
+})
 
+router.get('/test-api3', function (req, res) {
+    res.send({ msg : "hi functionUp "})
+})
+router.get('/test-api4', function (req, res) {
+    res.send({ msg : "hi functionUp , This is a fourth api ! " , name : "funcionUp" , age : 5})
+})
+router.get('/test-api5', function (req, res) {
+    res.send({ msg : [2,1,24,4,1,123,5]})
+})
+
+router.post('/test-post1' , function(req,res) {
+    
+res.send({msg : "post req 3"})
+console.log(req.body)
+});
+
+//write a post req to accept an element in post request body and add it to the given array and return the new array ?
+router.post('/test-post2' , function(req ,res){
+    let arr = [2,4,6,7]
+    let x = req.body.number
+    arr.push(x)
+res.send({msg : "my 2nd post req" , data : arr})
 });
 module.exports = router;
 // adding this comment for no reason
